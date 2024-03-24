d42cf5ccd0142b1bf27f2f0aae0778e40249540a
shin.suzuki <shin.suzuki.dev@gmail.com>
2024-03-24 15:52:37 +0900

    1st commit

---
+++ hello-project/src/components/WithModule.vue
@@ -0,0 +1,19 @@
+﻿<script setup lang="ts">
+import { ref } from "vue";
+
+const name = ref("nanasi");
+</script>
+
+<template>
+  <section>
+    <p>{{ name }} san!</p>
+    <input type="text" v-model="name" />
+  </section>
+</template>
+
+<style scoped>
+section {
+  border: orange 1px dashed;
+  margin: 10px;
+}
+</style>
